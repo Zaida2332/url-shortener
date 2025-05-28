a2388d0458d762b6e420a051274452a9e4542982
Zaida2332 <43aqaileh@gmail.com>
2025-05-28 14:13:49 +0300

    work

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express =require('express');
 const mongoose =require('mongoose');
 const urlRouter = require('./routes/uri');
-const cors = require("cors"); // Delete this
+const cors = require("cors"); // 
 const app =express();
 
 app.use(express.json());
-app.use(cors()) // Delete this
+app.use(cors()) // 
 mongoose.connect('mongodb://localhost/urlshortener').then(() =>console.log('mongodb connected'));
 app.use( '/', urlRouter);
 const PORT = 8000;
